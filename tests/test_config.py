82b7cee99e53774a87e8d54dfdc7e861c9a5c369
Arsen Gumin <aagumin@sberbank.ru>
2024-08-14 13:30:36 +0300

    chore: Setting the default spark_version value from pyspark.__version__

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,9 +1,17 @@
+import os
 from unittest import mock
 
 import pyspark
 import pytest
 
 from pydeequ.configs import _extract_major_minor_versions, _get_spark_version
+
+
+@pytest.fixture
+def mock_env(monkeypatch):
+    with mock.patch.dict(os.environ, clear=True):
+        monkeypatch.delenv("SPARK_VERSION", raising=False)
+        yield
 
 
 @pytest.mark.parametrize(
@@ -20,29 +28,17 @@ def test_extract_major_minor_versions(full_version, major_minor_version):
 
 
 @pytest.mark.parametrize(
-    "spark_version, expected",
-    [
-        ("3.2.1", "3.2"),
-        ("3.1", "3.1"),
-        ("3.10.3", "3.10"),
-        ("3.10", "3.10")
-    ]
+    "spark_version, expected", [("3.2.1", "3.2"), ("3.1", "3.1"), ("3.10.3", "3.10"), ("3.10", "3.10")]
 )
-def test__get_spark_version_without_cache(spark_version, expected):
+def test__get_spark_version_without_cache(spark_version, expected, mock_env):
     with mock.patch.object(pyspark, "__version__", spark_version):
-        _get_spark_version.cache_clear()
         assert _get_spark_version() == expected
+        _get_spark_version.cache_clear()
 
 
 @pytest.mark.parametrize(
-    "spark_version, expected",
-    [
-        ("3.2.1", "3.2"),
-        ("3.1", "3.2"),
-        ("3.10.3", "3.2"),
-        ("3.10", "3.2")
-    ]
+    "spark_version, expected", [("3.2.1", "3.2"), ("3.1", "3.2"), ("3.10.3", "3.2"), ("3.10", "3.2")]
 )
-def test__get_spark_version_with_cache(spark_version, expected):
+def test__get_spark_version_with_cache(spark_version, expected, mock_env):
     with mock.patch.object(pyspark, "__version__", spark_version):
         assert _get_spark_version() == expected
